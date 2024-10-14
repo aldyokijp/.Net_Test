e797cbdc2a9e7ce9cd9428296cae63041f455a74
alsra <alsra@ALGANTIC>
2024-10-14 10:42:07 +0700

    Add project files.

---
+++ CRUDTestApp/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace CRUDTestApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+
+}
